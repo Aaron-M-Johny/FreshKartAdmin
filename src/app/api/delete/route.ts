e3bd64e88f6932e59463fdfc13960636e0446b05
Aaron M Johny <aaron.professional.mj@gmail.com>
2025-06-02 19:11:45 +0530

    Fixed delete api error

--- src/app/api/delete/route.ts
+++ src/app/api/delete/route.ts
@@ -1,27 +1,36 @@
+// import { NextResponse } from "next/server";
+
+// export async function POST(req: Request) {
+//   const productIds: number[] = await req.json();
+
+//   const { prisma } = await import("@/lib/prisma"); // lazy import
+
+//   try {
+//     await Promise.all(
+//       productIds.map((ProductId) =>
+//         prisma.grocery.delete({
+//           where: { ProductId },
+//         })
+//       )
+//     );
+
+//     console.log(productIds);
+
+//     return NextResponse.json({ status: "success", message: "Deleted!" });
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json(
+//       { status: "error", message: "Failed to Delete." },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  // const productIds: number[] = await req.json();
-
-  // const { prisma } = await import("@/lib/prisma");
-
-  // try {
-  //   await Promise.all(
-  //     productIds.map((ProductId) =>
-  //       prisma.grocery.delete({
-  //         where: { ProductId },
-  //       })
-  //     )
-  //   );
-
-  //   console.log(productIds);
-
-  //   return NextResponse.json({ status: "success", message: "Deleted!" });
-  // } catch (error) {
-  //   console.error(error);
-    return NextResponse.json(
-      { status: "error", message: "Delete disabled by Admin" },
-      { status: 500 }
-    );
-  // }
+export async function POST() {
+  return NextResponse.json(
+    { status: "error", message: "Delete disabled by Admin" },
+    { status: 500 }
+  );
 }
